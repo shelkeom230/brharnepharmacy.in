b475facba390201741b5489fe70ff96b755b0cef
shelkeom230 <shelkeom230@gmail.com>
2023-06-18 13:22:46 +0530

    updated faculty profiles on faculty.php page without DP

--- faculty.php
+++ faculty.php
@@ -1,38 +1,97 @@
+<?php include("header.php") ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>faculty</title>
 </head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<style>
+    body{
+        overflow-x: hidden;
+    }
+</style>
 <body>
-    <?php
-    include "header.php";
-    ?>
-    
-    <div class="container-fluid mt-3">
+
+  <div class="container-fluid mt-3">
     <div class="row" style="margin-left: 80px;margin-right: 80px;">
-    <div class="col p-3" style="text-align: center;">
-       <br><br>
-      <img src="" style="width: 100%;height: 100%; border-radius: 50%;">
-    </div>
+      <div class="col p-3" style="text-align: center;">
+        <br><br>
+        <img src="assets/img/2.jpeg" style="width: 100%;height: 100%; border-radius: 50%;"><br><br>
+        Dr. Mukund G. Tawar<br>
+        M.Pharm Ph.D<br>
+        Principal<br>
+        <br>
+        <a href="assets/faculty/Dr. Mukund G. Tawar_Website.pdf" target="_blank;"><button style="background-color:#038681;color:white;border-radius:5px;border-color:white;">visit profile</button></a>
+      </div>
 
-    <div class="col p-3 " style="text-align: center"><br><br>
-      <img src="" style="width: 100%;height: 100%; border-radius: 50%;">
-      faculty
-    </div>
+      <div class="col p-3" style="text-align: center;">
+        <br><br>
+        <img src="assets/img/2.jpeg" style="width: 100%;height: 100%; border-radius: 50%;"><br><br>
+        Prof.Anuradha B. Sakhare<br>
+        M.Pharm (Pharmaceutics)<br>
+        Assistant Professor<br><br>
 
-    <div class="col p-3 " style="text-align: center"><br><br>
-      <img src="" style="width: 100%;height: 100%; border-radius:50%;">
-      faculty
+        <a href="assets/faculty/faculty Profile Format (1).pdf" target="_blank;"><button style="background-color:#038681;color:white;border-radius:5px;border-color:white;">visit profile</button></a>
+      </div>
+
+      <div class="col p-3" style="text-align: center;">
+        <br><br>
+        <img src="assets/img/2.jpeg" style="width: 100%;height: 100%; border-radius: 50%;"><br><br>
+        Prof.Snehal S. Magar<br>
+        B.Pharm<br>
+        Lecturer
+        <br><br>
+        <a href="assets/faculty/faculty Profile Format snehal magar.pdf" target="_blank;"><button style="background-color:#038681;color:white;border-radius:5px;border-color:white;">visit profile</button></a>
+      </div>
     </div>
   </div>
-    </div>
+  
+  <br><br><br><br><br><br><br>
+  
+  
+  <div class="container-fluid mt-3">
+    <div class="row" style="margin-left: 80px;margin-right: 80px;">
+      <div class="col p-3" style="text-align: center;">
+        <br><br>
+        <img src="assets/img/2.jpeg" style="width: 100%;height: 100%; border-radius: 50%;"><br><br>
+        Prof.Pooja Mahendrta Vani<br>
+        B.Pharm<br>
+        Lecturer<br>
+        <br>
+        <a href="assets/faculty/faculty Profile Formatpooja.pdf" target="_blank;"><button style="background-color:#038681;color:white;border-radius:5px;border-color:white;">visit profile</button></a>
+      </div>
 
-    <?php
-    include "footer.php";
-    ?>
-    
+      <div class="col p-3" style="text-align: center;">
+        <br><br>
+        <img src="assets/img/2.jpeg" style="width: 100%;height: 100%; border-radius: 50%;"><br><br>
+        Prof.Anuradha B. Sakhare<br>
+        M.Pharm (Pharmaceutics)<br>
+        Assistant Professor<br><br>
+        
+        <a href="assets/faculty/faculty Profile Format (1).pdf" target="_blank;"><button style="background-color:#038681;color:white;border-radius:5px;border-color:white;">visit profile</button></a>
+      </div>
+
+      <div class="col p-3" style="text-align: center;">
+        <br><br>
+        <img src="assets/img/2.jpeg" style="width: 100%;height: 100%; border-radius: 50%;"><br><br>
+        Prof.Snehal S. Magar<br>
+        B.Pharm<br>
+        Lecturer
+        <br><br>
+        <a href="assets/faculty/faculty Profile Format snehal magar.pdf" target="_blank;"><button style="background-color:#038681;color:white;border-radius:5px;border-color:white;">visit profile</button></a>
+      </div>
+    </div>
+  </div>
+  
+  
+  <br><br><br><br><br><br><br>
+  
+  <?php
+  include "footer.php";
+  ?>
+
 </body>
+
 </html>
